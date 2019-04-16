c1a3584bf56c92d271e5e783c1b4c7f09e678b7d
luiz azevedo <lwnazevedo@gmail.com>
2019-04-15 21:59:30 -0300

    Criado a casca do signIn

--- src/app/pages/auth/sign-in/sign-in.component.ts
+++ src/app/pages/auth/sign-in/sign-in.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CustomValidators } from 'ng5-validation';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sign-in',
@@ -7,9 +10,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignInComponent implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup;
+  emailTesting = 'dragons@dragons.com';
+  passwordTestig = 'dragons123';
+  submittingForm = false;
+
+  constructor(private fb: FormBuilder, private router: Router) { }
 
   ngOnInit() {
+    this.baseLoginForm()
   }
+
+  baseLoginForm() {
+    this.loginForm = this.fb.group({
+      'email': ['', [Validators.required, CustomValidators.email, CustomValidators.equal(this.emailTesting)]],
+      'password': ['', [Validators.required, CustomValidators.equal(this.passwordTestig)]]
+    });
+  }
+
+  get f() { return this.loginForm.controls; }
 
 }
